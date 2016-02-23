3a2ce3124cb9cb2c73504682f4c37a526295e853
Mario Vallejo <mario@vallgroup.com>
2016-02-23 16:32:18 -0600

    updated version 1.2.1
    fixes

--- premise-tabs.php
+++ premise-tabs.php
@@ -3,7 +3,7 @@
  * Plugin Name: Premise Tabs
  * Description: Responsive CSS Tabs. Premise-WP add-on.
  * Plugin URI:
- * Version:     2.0.0
+ * Version:     2.0.1
  * Author:      Vallgroup LLC
  * Author URI:  http://vallgroup.com
  * License:     GPL
